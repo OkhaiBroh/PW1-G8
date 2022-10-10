51865185ac5e8ce994da7b4f71218505cea2a002
Tomas Uzcudun <tomas.uzcudun@gmail.com>
2022-10-10 19:46:27 +0200

    Creacion del fichero register.css, modificacion del fichero de login css para utilizar algunas clases comunes. Agregada la view RegisterView.

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="row">
+        <div class="column center">
+            <p class="title"> Open Events </p>
+            <div class="bar-container">
+                <div class="line-container">
+                    <span class="circle"></span>
+                    <hr class="line">
+                    <span class="circle"></span>
+                </div>
+                <div class="text-container">
+                    <p class="bar-text1"> Account </p>
+                    <p class="bar-text2"> Security </p>
+                </div>
+            </div>
+            <div class="panel">
+                <div class="horizontal">
+                    <img class="ico" src="PNG/user_ico.png"/>
+                    <div class="input-form">
+                        <input name="name" placeholder="Name" type="text" required>
+                        <label for="name"> Name* </label>
+                    </div>
+                </div>
+                <div class="horizontal">
+                    <img class="ico" src="PNG/user_ico.png"/>
+                    <div class="input-form">
+                        <input name="lastname" placeholder="Lastname" type="text" required>
+                        <label for="lastname"> Lastname* </label>
+                    </div>
+                </div>
+                <div class="horizontal">
+                    <img class="ico" src="./SVG/ico_email.svg"/>
+                    <div class="input-form">
+                        <input class="text-input" name="email" placeholder="Email" type="email" required>
+                        <label class="label-input" for="email"> Email* </label>
+                    </div>
+                </div>
+                <div class="horizontal">
+                    <img class="ico" src="./SVG/ico_image.svg"/>
+                    <div class="input-form">
+                        <label class="label-file" for="file-upload"> Select profile picture... </label>
+                        <input id="file-upload" type="file">
+                    </div>
+                </div>
+                <button class="next"> Next </button>
+                <p class="login-text"> Registered? Log in </p>
+            </div>
+        </div>
+        <div class="column">
+            <img class="image" src="https://grandluxorhotels.com/wp-content/uploads/2016/09/9323706488_7c288a9659_b.jpg">
+        </div>
+    </div> 
+</template>
