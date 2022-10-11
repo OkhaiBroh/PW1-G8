42cdea4764da6275f7aba8a5b1c123c5f665a6fc
david.deu <david.deu@students.salle.url.edu>
2022-10-11 10:39:09 +0200

    structure plus basic css done

--- src/views/FriendsView.vue
+++ src/views/FriendsView.vue
@@ -1,3 +1,20 @@
-<template>
-    FriendsView
+<template class="background">
+    <div class="panel">
+        <p class="friends">Friends</p>
+
+        <div class="option">
+            <p class="option_text">Requests</p>
+            <img src="../svgs/ico_user.svg" class="ico">
+        </div>
+
+        <div class="option">
+            <p class="option_text">List</p>
+            <img src="../svgs/ico_user.svg" class="ico">
+        </div>
+
+      
+    </div>
+      <div class="panel">
+            
+        </div>
 </template>
