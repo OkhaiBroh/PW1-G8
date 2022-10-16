f4b923d238729af4e370cd8b8a739173f8a5d261
Arnau Ros <arnau.ros@students.salle.url.edu>
2022-10-16 10:58:28 +0200

    HTML structure

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -1,3 +1,61 @@
 <template>
-    EventsView
+    <div class="row">
+        <div class="sorter">
+            <p class="sort-by-text"> Sort By </p>
+            <button> Name </button>
+            <button> Date </button>
+            <button> Location </button>
+            <button> Rating </button>
+        </div>
+        <div class="searcher">
+            <section class="bar">
+                <button> Search... </button>
+                <button> Create </button>
+            </section>
+            <section class="elements">
+                <article>
+                    <img class="location1-img" src="../assets/icons/ico_user.svg"/>
+                    <p class="event-name1"> La Fiestita </p>
+                    <div class="location1">
+                        <img class="loc-ico" src="../assets/icons/ico_user.svg"/>
+                        <p class="location1-name"> Barcelona </p>
+                    </div>
+                    <div class="date1">
+                        <img class="date-ico" src="../assets/icons/ico_user.svg"/>
+                        <p class="start-date1"> 01/10/2022 </p>
+                        <p class="hyphen"> - </p>
+                        <p class="start-date1"> 05/10/2022 </p>
+                    </div>
+                </article>
+                <article>
+                    <img class="location1-img" src="../assets/icons/ico_user.svg"/>
+                    <p class="event-name1"> La Fiestita </p>
+                    <div class="location1">
+                        <img class="loc-ico" src="../assets/icons/ico_user.svg"/>
+                        <p class="location1-name"> Barcelona </p>
+                    </div>
+                    <div class="date1">
+                        <img class="date-ico" src="../assets/icons/ico_user.svg"/>
+                        <p class="start-date1"> 01/10/2022 </p>
+                        <p class="hyphen"> - </p>
+                        <p class="start-date1"> 05/10/2022 </p>
+                    </div>
+                </article>
+                <article>
+                    <img class="location1-img" src="../assets/icons/ico_user.svg"/>
+                    <p class="event-name1"> La Fiestita </p>
+                    <div class="location1">
+                        <img class="loc-ico" src="../assets/icons/ico_user.svg"/>
+                        <p class="location1-name"> Barcelona </p>
+                    </div>
+                    <div class="date1">
+                        <img class="date-ico" src="../assets/icons/ico_user.svg"/>
+                        <p class="start-date1"> 01/10/2022 </p>
+                        <p class="hyphen"> - </p>
+                        <p class="start-date1"> 05/10/2022 </p>
+                    </div>
+                </article>
+            </section>    
+        </div>
+    </div>
 </template>
