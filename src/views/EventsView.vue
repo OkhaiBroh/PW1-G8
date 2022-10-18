719f04b7ec6c3a38d6d9314b04deca821ea459ed
arnau.ros <arnau.ros@students.salle.url.edu>
2022-10-19 01:45:40 +0200

    Sorter CSS

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -48,6 +48,55 @@
         flex-direction: row;
     }
 
+    /*************************
+             SORTER
+    **************************/
+
+    .sorter-pack {
+        display: flex;
+        justify-content: space-between;
+
+        margin-top: 40px;
+        margin-right: 30px;
+    }
+
+    .sorter-panel {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        border-radius: 20px;
+
+        background-color: white;
+        padding: 30px 30px 10px 30px;
+    }
+
+    .sort-by-text {
+        font-weight: 900;
+        font-size: 25px;
+    }
+
+    .name-sort-but, .date-sort-but, .location-sort-but, .rating-sort-but {
+        border: 2px solid var(--bg_button); 
+        font-weight: 700;
+        height: 40px;
+        width: 150px;
+        margin: 20px 0 0 0;
+    }
+
+    .name-sort-but, .date-sort-but, .location-sort-but {
+        background-color: white;
+        color: black;
+    }
+
+    .name-sort-but {
+        margin: 40px 0 0 0;
+    }
+
+    .rating-sort-but {
+        margin: 20px 0 50px 0;
+    }
+
     
     
 </style>
