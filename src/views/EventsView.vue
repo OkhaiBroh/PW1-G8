a75916333e071eb5777f5da73f92e2c65cb3a6ef
arnau.ros <arnau.ros@students.salle.url.edu>
2022-10-19 10:58:28 +0200

    Events section HTML + CSS

--- src/views/EventsView.vue
+++ src/views/EventsView.vue
@@ -19,7 +19,42 @@
                     <button class="create-btn"> Create </button>
                 </div>
                 <div class="events-panel">
-                
+                    <article class="event-container box-shadow">
+                        <img class="event-image" src="https://st.depositphotos.com/1053646/1770/i/950/depositphotos_17700789-stock-photo-dance-club.jpg" alt="">
+                        <p class="timeline-title-text"> La Fiestita </p>
+                        <div class="info-container">
+                            <img class="ico" src="../assets/icons/ico_location.svg" alt="location ico">
+                            <p class="timeline-info-text"> Barcelona </p>
+                        </div>
+                        <div class="info-container">
+                            <img class="ico" src="../assets/icons/ico_calendar.svg" alt="calendar ico">
+                            <p class="timeline-info-text"> 01/10/2022 - 05/10/2022 </p>
+                        </div>
+                    </article>
+                    <article class="event-container box-shadow">
+                        <img class="event-image" src="https://st.depositphotos.com/1053646/1770/i/950/depositphotos_17700789-stock-photo-dance-club.jpg" alt="">
+                        <p class="timeline-title-text"> La Fiestita </p>
+                        <div class="info-container">
+                            <img class="ico" src="../assets/icons/ico_location.svg" alt="location ico">
+                            <p class="timeline-info-text"> Barcelona </p>
+                        </div>
+                        <div class="info-container">
+                            <img class="ico" src="../assets/icons/ico_calendar.svg" alt="calendar ico">
+                            <p class="timeline-info-text"> 01/10/2022 - 05/10/2022 </p>
+                        </div>
+                    </article>
+                    <article class="event-container box-shadow">
+                        <img class="event-image" src="https://st.depositphotos.com/1053646/1770/i/950/depositphotos_17700789-stock-photo-dance-club.jpg" alt="">
+                        <p class="timeline-title-text"> La Fiestita </p>
+                        <div class="info-container">
+                            <img class="ico" src="../assets/icons/ico_location.svg" alt="location ico">
+                            <p class="timeline-info-text"> Barcelona </p>
+                        </div>
+                        <div class="info-container">
+                            <img class="ico" src="../assets/icons/ico_calendar.svg" alt="calendar ico">
+                            <p class="timeline-info-text"> 01/10/2022 - 05/10/2022 </p>
+                        </div>
+                    </article>
                 </div>
             </section>
         </div>
@@ -108,7 +143,7 @@
         align-items: center;
 
         background-color: white;
-        padding: 30px 40px 10px 40px;
+        padding: 30px 40px 20px 40px;
 
         border-radius: 20px;
         margin-top: 40px;
@@ -159,5 +194,55 @@
         margin: 6px 18px 2px 0;
     }
 
+    /*************************
+            EVENTS
+    **************************/
+
+    .events-panel {
+        display: flex;
+        justify-content: space-evenly;
+    }
+
+    .event-container {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        margin: 25px;
+        border-radius: 25px;
+        padding: 20px;
+    }
+
+    .box-shadow {
+        box-shadow: 0px 30px 80px 10px rgba(0, 0, 0, 0.1);
+    }
+
+     .event-image {
+        max-width: 145px;
+        border-radius: 15px;
+    }
+
+    .timeline-title-text {
+        font-weight: 700;
+        font-size: 16px;
+        margin-top: 10px;
+    }
+
+    .timeline-info-text {
+        font-size: 12px;
+    }
+
+    .ico {
+        width: 15px;
+        height: 15px;
+        margin-right: 10px;
+    }
+
+    .info-container {
+        display: flex;
+        justify-content: center;
+        flex-direction: row;
+        margin-top: 10px;
+    }
     
 </style>
