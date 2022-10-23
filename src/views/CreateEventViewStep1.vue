5598cf4a224c9620cceb9d1a59ddbf90174961d3
Arnau Ros <arnau.ros@students.salle.url.edu>
2022-10-23 20:43:07 +0200

    Basic HTML

---
+++ src/views/CreateEventViewStep1.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="background">
+        <div class="general-container">
+            <div class="main-panel">
+                <div class="title-and-bar">
+                </div>
+                <section class="data-input">
+                </section>
+                <div class="buttons">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
