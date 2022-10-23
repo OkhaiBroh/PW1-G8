ebd97719d4fb5d9caa52aa924a88bf295b16584b
Arnau Ros <arnau.ros@students.salle.url.edu>
2022-10-23 16:39:18 +0200

    Basic structure (HTML)

---
+++ src/views/OtherProfileView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="background">
+        <div class="general-container">
+            <section class="profile-section">
+                Profile
+            </section>
+            <section class="data-section">
+                Personal Data
+            </section>
+        </div>
+    </div>
+</template>
