b3948229c56c5d681a5027899b1f6bee75817a97
Arnau Ros <arnau.ros@students.salle.url.edu>
2022-10-23 20:48:16 +0200

    Adding a bit of HTML structure + basic CSS

--- src/views/CreateEventViewStep1.vue
+++ src/views/CreateEventViewStep1.vue
@@ -3,8 +3,14 @@
         <div class="general-container">
             <div class="main-panel">
                 <div class="title-and-bar">
+                    <p class="create-event-title"> Create Event </p>
+                    <div class="progress-bar">
+                    </div>
                 </div>
                 <section class="data-input">
+                    <div class="name-description-camps">
+                    </div>
+
                 </section>
                 <div class="buttons">
                 </div>
@@ -12,3 +18,27 @@
         </div>
     </div>
 </template>
+
+<style scoped>
+
+    html,
+    body {
+        margin: 0px;
+        height: 100%;
+    }
+    
+    .background {
+        background-color: var(--gray_color);
+        display: flex;
+        justify-content: center;
+        height: 100vh;
+    }
+
+    .general-container {
+        width: fit-content;
+        height: fit-content;
+        display: flex;
+        flex-direction: row;
+    }
+    
+</style>
