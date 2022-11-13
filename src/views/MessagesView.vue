d818716df1bbc31b798fe2e1883636f5c1ff4e84
Renzo Luna <renzo.luna.liendo@gmail.com>
2022-11-13 14:03:43 +0100

    responsive in progress, borders to 0px

--- src/views/MessagesView.vue
+++ src/views/MessagesView.vue
@@ -1,57 +1,59 @@
 <template>
     <div class="background">
-        <div class="left">
-            <input class="input_search" type="text" placeholder="search"/>
-            <ul class="list_chats">
-                <li>
-                    <div class="user_chat">
-                        <button class="button_chat">
-                            <img class="ico" src="../assets/icons/ico_user_chat.svg"/>
-                            <p class="p_style"> Tomas Uzcudun </p>
-                        </button>
-                    </div>
-                    <div class="user_chat">
-                        <button class="button_chat">
-                            <img class="ico" src="../assets/icons/ico_user_chat.svg"/>
-                            <p class="p_style"> Arnau Ros </p>
-                        </button>
-                    </div>
-                    <div class="user_chat">
-                        <button class="button_chat">
-                            <img class="ico" src="../assets/icons/ico_user_chat.svg"/>
-                            <p class="p_style"> David Deu </p>
-                        </button>
-                    </div>
-                    <div class="user_chat">
-                        <button class="button_chat">
-                            <img class="ico" src="../assets/icons/ico_user_chat.svg"/>
-                            <p class="p_style"> Open Events </p>
-                        </button>
-                    </div>
-                </li>
-            </ul>
-        </div>
-        <div class="right">
-            <div class="ico-and-name">
-                <img class="my_ico" src="../assets/icons/ico_user_chat.svg"/>
-                <p class="p_style_user"> Arnau Ros Sánchez </p>
+        <div class="chat_box">
+            <div class="left">
+                <input class="input_search" type="text" placeholder="search"/>
+                <ul class="list_chats">
+                    <li>
+                        <div class="user_chat">
+                            <button class="button_chat">
+                                <img class="ico" src="../assets/icons/ico_user_chat.svg"/>
+                                <p class="p_style"> Tomas Uzcudun </p>
+                            </button>
+                        </div>
+                        <div class="user_chat">
+                            <button class="button_chat">
+                                <img class="ico" src="../assets/icons/ico_user_chat.svg"/>
+                                <p class="p_style"> Arnau Ros </p>
+                            </button>
+                        </div>
+                        <div class="user_chat">
+                            <button class="button_chat">
+                                <img class="ico" src="../assets/icons/ico_user_chat.svg"/>
+                                <p class="p_style"> David Deu </p>
+                            </button>
+                        </div>
+                        <div class="user_chat">
+                            <button class="button_chat">
+                                <img class="ico" src="../assets/icons/ico_user_chat.svg"/>
+                                <p class="p_style"> Open Events </p>
+                            </button>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <hr class="line">
-            <div class="chat">
-                <div class="message_cotainer">
-                    <div class="message-background my-message-background">
-                        <p class="message-text"> Un Mensaje </p>
+            <div class="right">
+                <div class="ico-and-name">
+                    <img class="my_ico" src="../assets/icons/ico_user_chat.svg"/>
+                    <p class="p_style_user"> Arnau Ros Sánchez </p>
+                </div>
+                <hr class="line">
+                <div class="chat">
+                    <div class="message_cotainer">
+                        <div class="message-background my-message-background">
+                            <p class="message-text"> Un Mensaje </p>
+                        </div>
+                    </div>
+                    <div class="message_cotainer">
+                        <div class="message-background other-message-background">
+                            <p class="message-text"> Otro Mensaje </p>
+                        </div>
                     </div>
                 </div>
-                <div class="message_cotainer">
-                    <div class="message-background other-message-background">
-                        <p class="message-text"> Otro Mensaje </p>
-                    </div>
+                <div class="write">
+                    <input class="message-input" type="text" autocomplete="on" placeholder="Message..."/>
+                    <button class="button_ico"></button>
                 </div>
-            </div>
-            <div class="write">
-                <input class="message-input" type="text" autocomplete="on" placeholder="Message..."/>
-                <button class="button_ico"></button>
             </div>
         </div>
     </div>
@@ -68,8 +70,58 @@
         display: flex;
         justify-content: center;
         align-content: center;
-        padding: 10vh;
+        padding: 5vh;
         background-color: var(--light_gray_color);
+    }
+
+    .chat_box{
+        height: auto;
+        flex: 1 1 auto;
+        flex-wrap: wrap;
+        min-height: 0;
+        min-width: 0;
+        justify-content: center;
+        flex-direction: row;
+        align-items: stretch;
+        align-content: stretch;
+        display: flex;
+    }
+
+    /****************************************
+    *                Right Side              *
+    *****************************************/
+
+    .right {
+        padding: 5px 50px 25px 50px;
+        border-radius: 0px;
+        margin-left: 2vw;
+        margin-top: 2vw;
+        margin-right: 2vw;
+        margin-bottom: 2vw;
+        background-color: white;
+        display: flex;
+        flex-direction: column;
+        width: 50%;
+        box-shadow: 0px 30px 80px 10px rgba(0, 0, 0, 0.1);
+    }
+
+    /****************************************
+    *                Left Side              *
+    *****************************************/
+
+    .left {
+        display: flex;
+        margin-left: 2vw;
+        margin-top: 2vw;
+        margin-right: 2vw;
+        margin-bottom: 2vw;
+        align-items: center;
+        flex-direction: column;
+        align-items: stretch;
+        align-content: stretch;
+        background-color: white;
+        border-radius: 0px;
+        box-shadow: 0px 30px 80px 10px rgba(0, 0, 0, 0.1);
     }
 
     /****************************************
@@ -112,7 +164,6 @@
         flex-direction: column;
         width: 100%;
         height: 70%;
-
         flex-grow: 1;
     }
 
@@ -149,67 +200,42 @@
     }
 
     /****************************************
-    *                Right Side              *
-    *****************************************/
-
-    .right {
-        width: 50%;
-        padding: 5px 50px 25px 50px;
-        border-radius: 20px;
-        margin-left: 5vw;
-        background-color: white;
-
-        display: flex;
-        flex-direction: column;
-
-        box-shadow: 0px 30px 80px 10px rgba(0, 0, 0, 0.1);
-    }
-
-    /****************************************
-    *                Left Side              *
-    *****************************************/
-
-    .left {
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-        background-color: white;
-
-        border-radius: 20px;
-
-        box-shadow: 0px 30px 80px 10px rgba(0, 0, 0, 0.1);
-
-        padding: 20px;
-    }
-
-    /****************************************
     *             Chat users                *
     *****************************************/
     .list_chats{
         list-style: none;
-        width: 100%;
+        display: flex;
+        height: auto;
     }
 
     .input_search{
-        width: 250px;
         border: none;
         background-color: var(--light_gray_color);
-        padding: 20px;
+        height: 50px;
         outline: none;
-        border-radius: 20px;
-        margin-bottom: 25px;
+        border-radius: 0px;
+        padding: 0px 0px 0px 5px;
     }
     
     .user_chat{
         display: flex;
         align-items: center;
-        width: 100%;
         height: 50px;
-        padding-top: 10px;
     }
-    
+
+    .button_chat{
+        background-color: var(--light_gray_color);
+        display: flex;
+        border: 1px solid var(--white_color);
+        align-items: stretch;
+        cursor: pointer;
+        flex-direction: row;
+        align-content: stretch;
+        border-radius: 0px;
+        font-size: 100%;
+    }
+
     .p_style {
-        font-size: 20px;
         color: #000;
         padding-left: 20px;
     }
@@ -217,20 +243,6 @@
     .ico {
         width: 35px;
         height: 30px;
-    }
-
-    .button_chat{
-        background-color: var(--light_gray_color);
-        display: flex;
-        border: 1px solid var(--white_color);
-        align-items: center;
-        cursor: pointer;
-        flex-direction: row;
-        width: 100%;
-        height: 50px;
-        border-radius: 0px;
-        margin-bottom: 35px;
-        border-radius: 20px;
     }
 
     /****************************************
